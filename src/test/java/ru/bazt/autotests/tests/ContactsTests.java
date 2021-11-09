8ac2eff17857b77fc250184398cb73f3226d1bcc
Noi8E <ftretyakov@kosmostyt.ru>
2021-11-09 10:17:45 +0300

    добавил тест не контакты

--- src/test/java/ru/bazt/autotests/tests/ContactsTests.java
+++ src/test/java/ru/bazt/autotests/tests/ContactsTests.java
@@ -1,4 +1,31 @@
 package ru.bazt.autotests.tests;
 
+import io.qameta.allure.Description;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static io.qameta.allure.Allure.step;
+
 public class ContactsTests extends TestBase {
+
+    @Test
+    @Description("ContactsTests")
+    @DisplayName("Страница с контактами работает")
+    void contactsTests() {
+        step("Open main page", () ->
+                open(mainUrl));
+        step("На странице отображается шапка", () -> {
+            $(".header_auto").should(visible);
+        });
+        step("Кликнуть на кнопку с контактами", () -> {
+            $(".navigation").should(visible);
+            $("a[href='contacts']").click();
+        });
+        step("На странце контактов отображается корректный номер телефона", () -> {
+            $x("//div[@class='content']/div[@class='telephone']").shouldHave(text("+7 (495) 646 11 75"));
+        });
+    }
 }
