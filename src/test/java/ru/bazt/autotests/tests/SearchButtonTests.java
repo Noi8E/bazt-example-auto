193740847eeb75b2d67128b937fcefe5fb3c1094
Noi8E <ftretyakov@kosmostyt.ru>
2021-11-24 21:47:34 +0300

    добил тесты по поиску

--- src/test/java/ru/bazt/autotests/tests/SearchButtonTests.java
+++ src/test/java/ru/bazt/autotests/tests/SearchButtonTests.java
@@ -11,8 +11,8 @@ import static io.qameta.allure.Allure.step;
 public class SearchButtonTests extends TestBase {
     @Test
     @Description("Поиск работает")
-    @DisplayName("Поиск работает на разных сценариях")
-    void searchCanShowResultByCorrectRequest() {
+    @DisplayName("Поиск работает на пустом значении")
+    void searchCanWorkOnEmptyRequest() {
         step("Open main page", () ->
                 open(mainUrl));
         step("Click on Search - button", () ->
@@ -20,12 +20,43 @@ public class SearchButtonTests extends TestBase {
         step("Check available search - form", () ->
                 $(".sisea-search-form").shouldBe(visible).click());
         step("Put in empty string in search form", () ->
-                $("#search").setValue(""));
+                $("#search").setValue("").pressEnter());
         step("Check empty result", () ->
                 $x("//*[contains(text(),'По вашему запросу ничего не найдено.')]").shouldBe(visible));
 
+    }
 
+    @Test
+    @Description("Поиск работает")
+    @DisplayName("Поиск работает на валидном значении")
+    void searchCanWorkOnNonEmptyRequest() {
+        step("Open main page", () ->
+                open(mainUrl));
+        step("Click on Search - button", () ->
+                $(".search_button").shouldBe(visible).click());
+        step("Check available search - form", () ->
+                $(".sisea-search-form").shouldBe(visible).click());
+        step("Put in empty string in search form", () ->
+                $("#search").setValue("тест").pressEnter());
+        step("Check valid result", () ->
+                $x("//*[contains(text(),'9 результатов найдено')]").shouldBe(visible));
 
+    }
+
+    @Test
+    @Description("Поиск работает")
+    @DisplayName("Поиск работает при вводе XSS")
+    void searchCanWorkOnXSSInjection() {
+        step("Open main page", () ->
+                open(mainUrl));
+        step("Click on Search - button", () ->
+                $(".search_button").shouldBe(visible).click());
+        step("Check available search - form", () ->
+                $(".sisea-search-form").shouldBe(visible).click());
+        step("Put in empty string in search form", () ->
+                $("#search").setValue("<script >alert(\"Hello\");</script > \n").pressEnter());
+        step("Check xss result", () ->
+                $("[value = 'alert(\"Hello\"); ' ]").shouldBe(visible));
 
     }
 
